64785813e9bf17843121073899b7e6d05a025a02
Ryan Marvin <marvinryan@ymail.com>
2017-04-10 15:32:46 +0300

    [Feature] Add api

--- nurses.py
+++ nurses.py
@@ -1,17 +1,23 @@
 from bs4 import BeautifulSoup
 from config import NURSING_COUNCIL_URL
+from flask import Flask, request
+from werkzeug.exceptions import HTTPException, default_exceptions
 import requests
 import json
+
+
+app = Flask(__name__)
 
 nurse_fields = ["name", "licence_no", "valid_till"]
 
 
-def find_nurse(event, context):
+@app.route('/api/find_nurse', methods=['GET'])
+def find_nurse():
     try:
-        query = event["query"]
-        if len(query) < 1:
+        query = request.args.get('q')
+        if not query or len(query) < 1:
             return json.dumps({
-                              "error": "Query cannot be empty.",
+                              "error": "A query is required.",
                               "results": ""
                               })
         url = NURSING_COUNCIL_URL.format(query)
@@ -42,3 +48,23 @@ def find_nurse(event, context):
                           "error": str(err),
                           "results": ""
                           })
+
+
+def handle_error(error):
+    '''Generic error handlers for all http exceptions'''
+    response = {}
+    status_code = 500
+    if isinstance(error, HTTPException):
+        status_code = error.code
+    response["status_code"] = status_code
+    response["error"] = str(error)
+    response['description'] = error.description
+    return json.dumps(response), status_code
+
+
+# change error handler for all http exceptions to return json instead of html
+for code in default_exceptions.keys():
+    app.errorhandler(code)(handle_error)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
