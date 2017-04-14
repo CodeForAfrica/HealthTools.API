6d559e9aa6d26d0dde37fe8f8e33bf19057d45d3
Ryan Marvin <marvinryan@ymail.com>
2017-04-14 15:16:07 +0300

    [Fix] Add SMS configs

--- api_healthtools_ke/config.py
+++ api_healthtools_ke/config.py
@@ -1,5 +1,14 @@
 import os
 
 NURSING_COUNCIL_URL = "http://nckenya.com/services/search.php?p=1&s={}"
+
+# Url of memcached server
 MEMCACHED_URL = os.getenv("MEMCACHED_URL")
+
+# Google Analytics tracking id
 GA_TRACKING_ID = os.environ.get('GA_TRACKING_ID')
+
+# SMS provider credentials
+SMS_USER = os.environ.get('SMS_USER')
+SMS_PASS = os.environ.get('SMS_PASS')
+SMS_SHORTCODE = os.environ.get('SMS_SHORTCODE')
