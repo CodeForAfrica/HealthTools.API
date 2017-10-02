744ba96e6672bb3c714375da2218a55adf61187a
TinaMorale <tina.murimi@andela.com>
2017-10-02 15:37:52 +0300

    Message when doc_type not provided

--- healthtools/views/base_api.py
+++ healthtools/views/base_api.py
@@ -7,4 +7,5 @@ blueprint = Blueprint('base_api', __name__)
 @blueprint.route('/')
 def index():
     # TODO: Redirect to HealthTools docs instead
-    return redirect('https://github.com/CodeForAfricaLabs/HealthTools.API')
+    # return redirect('https://github.com/CodeForAfricaLabs/HealthTools.API')
+    return "Index page"
