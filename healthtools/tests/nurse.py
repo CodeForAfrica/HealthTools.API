b462139a495fc81a030c94c1f700a8a18006a6e4
Awinja-Andela <joan.awinja@andela.com>
2017-10-05 17:01:20 +0300

    added nurses

--- healthtools/tests/nurse.py
+++ healthtools/tests/nurse.py
@@ -24,14 +24,17 @@ class TestNurseRegistery(TestSetup):
     def test_gets_nurses_from_nc_registry_handle_inexistent_nurse(self):
         nurses = get_nurses_from_nc_registry("ihoafiho39023u8")
         self.assertEqual(len(nurses), 0)
-        self.assertIn
-    
+
 
 class TestNursesAPI(TestSetup):
     """
     This tests nurses search api with defined doctype
     """
+
     def test_nurses_endpoint_handles_bad_query(self):
+        """
+        This tests running nurses endpoint with valid doctype and no query
+        """
         response = self.client.get("search/nurses?q=")
         self.assertIn(b'"status": "FAILED"', response.data)
 
@@ -46,11 +49,25 @@ class TestNursesAPI(TestSetup):
         response = self.client.get("search/nurses?q=Marie")
         self.assertIn(b"X-Retrieved-From-Cache", response.headers.keys())
 
+    def test_nurses_endpoint_with_bad_endpoint(self):
+        """
+        This tests running an endpoint with incorrect/unavailable doctype 
+        """
+        response = self.client.get("search/nurse?q=Marie")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
+    def test_nurses_endpoint_with_unavailable_query(self):
+        """
+        This tests running nurses endpoint with correct doctype but unavailable query
+        """
+        response = self.client.get("search/nurses?q=1234")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
 class TestNursesAPIWithoutDoctypes(TestSetup):
     """
-    This tests nurses search api using keywords
+    This tests nurses search api without doctype, keywords are used instead
     """
-    def test_nurses_endpoint_handles_bad_query(self):
+    def test_nurses_endpoint_endpoint_without_keyword_in_query(self):
         response = self.client.get("search?q=Marie")
         self.assertIn(b'"status": "FAILED"', response.data)
 
@@ -58,4 +75,33 @@ class TestNursesAPIWithoutDoctypes(TestSetup):
         response = self.client.get("search?q=nurse Marie")
         self.assertIn(b"OK", response.data)
         self.assertIn(b"MARIE   WANJUGU  MURIGO", response.data)
+
+    def test_nurses_endpoint_with_unavailable_query(self):
+        """
+        This tests running nurses endpoint with correct available keyword but unavailable query
+        """
+        response = self.client.get("search?q=Registered Nurse 1234")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
+    def test_nurses_cs_endpoint_with_keyword_only(self):
+        """
+        This tests running nurses endpoint with correct available keyword only
+        """
+        response = self.client.get("search?q=nursing officer")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
+    def test_nurses_endpoint_without_query(self):
+        """
+        This tests running nurses endpoint without query
+        """
+        response = self.client.get("search?q=")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
+    def test_nurses_endpoint_with_nonkeyword(self):
+        """
+        This tests running nurses endpoint with a keyword that is unavailable.
+        """
+        response = self.client.get("search?q=kijana Marie")
+        self.assertIn(b'"status": "FAILED"', response.data)
+
     
