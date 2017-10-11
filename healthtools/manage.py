47f04bfb8027fbcf987d813370963f4868fa6fca
Awinja-Andela <joan.awinja@andela.com>
2017-10-11 08:09:57 +0300

    removed unnecessary lines from manage.py

--- healthtools/manage.py
+++ healthtools/manage.py
@@ -1,10 +1,3 @@
-import os
-import inspect
-import sys
-currentdir = os.path.dirname(os.path.abspath(
-    inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
 import logging
 
 from healthtools.core import create_app
