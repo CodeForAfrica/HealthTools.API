50e5a485be1342c17e514973b502e8e15b6ef77e
Awinja-Andela <joan.awinja@andela.com>
2017-10-19 12:41:10 +0300

    removed new url, modified to use doc type instead

--- healthtools/views/search_api.py
+++ healthtools/views/search_api.py
@@ -6,15 +6,15 @@ from healthtools.search.wit_ai import wit_run_query
 blueprint = Blueprint('search_api', __name__)
 
 @blueprint.route('/search', methods=['GET'], strict_slashes=False)
-@blueprint.route('/search/<search_parameter>', methods=['GET'], strict_slashes=False)
+# @blueprint.route('/search/<search_parameter>', methods=['GET'], strict_slashes=False)
 @blueprint.route('/search/<doc_type>', methods=['GET'], strict_slashes=False)
-def index(search_parameter=None, doc_type=None):
+def index(doc_type=None):
     query = request.args.get('q')
-    if search_parameter:
-        if search_parameter == 'wit':
-            result, doc_type = wit_run_query(query, doc_type)
-    else:
-        result, doc_type = run_query(query, doc_type)
+
+    if doc_type and doc_type == 'wit':
+        result, doc_type = wit_run_query(query)
+
+    result, doc_type = run_query(query, doc_type)
 
     # Error with run_query (run_query returns false)
     if not result:
