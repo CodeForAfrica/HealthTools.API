905f32a3e618278931d018c6b6bb75198af28ebb
Awinja-Andela <joan.awinja@andela.com>
2017-10-23 16:38:15 +0300

    added bot.py

--- healthtools/views/bot.py
+++ healthtools/views/bot.py
@@ -1,16 +1,12 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, request
 from healthtools.bot import messenger_bot
-import requests
-
 
 blueprint = Blueprint('bot', __name__)
 
 @blueprint.route('/bot/<adapter>', methods=['GET', 'POST'], strict_slashes=False)
 def index(adapter='messenger'):
-    if requests.method == "POST" :
+    if request.method == 'POST' :
         return messenger_bot.handle_messages
     else:
         return messenger_bot.handle_verification
-        
-        
-        
+          
