92482d3b9b65e4f3a15185ad302d3389e99e4092
Mayowa Makinde <mayowa.makinde@andela.com>
2017-11-16 13:35:14 +0100

    reword sms sent to user

--- healthtools/sms/__init__.py
+++ healthtools/sms/__init__.py
@@ -53,10 +53,11 @@ def create_sms(result, doc_type):
         return response
 
     # TODO: Figure out singular vs plural
-    response += 'We found ' + str(len(result['hits'])) + ' matches:'
+    response += 'Here are the top 3 matches your query returned'
     for hit in result['hits'][:3]:
 
-        response += '\n' + "{}. {}".format(str(result_count), hit['_source']['name'])
+        response += '\n' + '{}. {}'.format(str(result_count), hit['_source']['name'])
         result_count += 1
+    response += '\nFind the full list at http://health.the-star.co.ke/'
     log.info(response)
     return response
