8373fc644a4a1a59342dcb6cd9f3030e64795643
David Lemayian <dlemayian@gmail.com>
2018-05-07 16:23:33 +0300

    Switching back to pip freeze
    Looks like there are issues with pipreqs

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='healthtools',
-    version='0.0.2',
+    version='0.0.3',
     keywords='healthtools',
     author='Code for Africa',
     author_email='support@codeforafrica.org',
-    url='https://healthtools.readthedocs.io/',
+    url='https://github.com/CodeForAfricaLabs/HealthTools.API',
     license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
     include_package_data=True,
@@ -15,6 +15,8 @@ setup(
         'requests',
         'bs4',
         'elasticsearch',
-        'requests_aws4auth'
+        'requests_aws4auth',
+        'gunicorn',
+        'wit'
     ],
 )
