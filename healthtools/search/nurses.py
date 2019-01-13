db3b51f05af5b3b3575d7ac91c1c5186983b20d0
David Lemayian <dlemayian@gmail.com>
2019-01-13 21:29:10 +0300

    Fix Nurses search.

--- healthtools/search/nurses.py
+++ healthtools/search/nurses.py
@@ -3,43 +3,42 @@ import logging
 from bs4 import BeautifulSoup
 
 
-NURSING_COUNCIL_URL = 'http://nckenya.com/services/search.php?p=1&s={}'
-NURSES_FIELDS = ['name', 'licence_no', 'valid_till']
+FETCH_URL = 'https://osp.nckenya.com/ajax/public'
+TABLE_FIELDS = ['name', 'licence_no', 'valid_till']
 
 log = logging.getLogger(__name__)
 
 def search(query):
-    results = get_nurses_from_nc_registry(query)
+    results = get_people(query)
     return results
 
 
-def get_nurses_from_nc_registry(query):
+def get_people(query):
     '''
-    Get nurses from the nursing council of Kenya registry
+    Get people from the Register 
     '''
-    url = NURSING_COUNCIL_URL.format(query)
-    nurses = {'hits': [], 'total': 0}
+    people = {'hits': [], 'total': 0}
     try:
-        response = requests.get(url)
+        response = requests.post(FETCH_URL, data = {'search_register':'1', 'search_text': query})
         if 'No results' in response.content:
-            return nurses
+            return people
 
         # make soup for parsing out of response and get the table
         soup = BeautifulSoup(response.content, 'html.parser')
-        table = soup.find('table', {'class': 'zebra'}).find('tbody')
+        table = soup.find('table', {'class': 'datatables'}).find('tbody')
         rows = table.find_all("tr")
 
-        # parse table for the nurses data
+        # parse table for the people data
         for row in rows:
             # only the columns we want
-            columns = row.find_all('td')[:len(NURSES_FIELDS)]
+            columns = row.find_all('td')[:len(TABLE_FIELDS)]
             columns = [text.text.strip() for text in columns]
 
-            entry = dict(zip(NURSES_FIELDS, columns))
-            nurses['hits'].append(entry)
+            entry = dict(zip(TABLE_FIELDS, columns))
+            people['hits'].append(entry)
 
-        nurses['total'] = len(nurses['hits'])
+        people['total'] = len(people['hits'])
 
-        return nurses
+        return people
     except Exception as err:
-        log.error("Error getting nurses from the nursing council url \n" + str(err))
+        log.error("Error getting people from the register \n" + str(err))
